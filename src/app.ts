71825846ff471f0811dc42355d0310a7ead119ef
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-07-17 11:47:30 +0600

    complete

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,15 @@ const app: Application = express();
 //middlewares
 app.use(express.json());
 app.use(cors());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5000"];
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+  })
+);
 
 //routes
 app.use("/api/v1/products", productRoute);
